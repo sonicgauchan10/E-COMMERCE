07f628a7ce287529bde65b3d1143a69396a60138
sonicgauchan10 <126335102+sonicgauchan10@users.noreply.github.com>
2025-02-25 13:06:16 +0545

    Delete delete.py

--- delete.py
+++
@@ -1,4 +0,0 @@
-# SQL: "DELETE FROM "table_name" where id = ? 
-# role: admin, customer, employee
-# table: customer -> {ID, Name, Phone no., Email, Password, Gender, Date of Birth}
-#          item -> {ID, name, price, description, image}
